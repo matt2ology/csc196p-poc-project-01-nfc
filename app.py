c9d4c39f6d5ed93770df639e50c09abfc7d0bd1a
Matt Tran <34324060+matt2ology@users.noreply.github.com>
2024-03-25 03:45:35 -0700

    init commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+GPIO.setwarnings(False)
+rfid = SimpleMFRC522()
+
+while True:
+        id, text = rfid.read()
+        print("id: " + str(id))
+        print("text: " + str(text))
