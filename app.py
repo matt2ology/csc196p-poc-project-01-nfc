17295916d61cf01d636f17cecbedb49f97aefbd9
matt2ology <34324060+matt2ology@users.noreply.github.com>
2024-03-25 03:49:40 -0700

    Formatted with autopep8

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ GPIO.setwarnings(False)
 rfid = SimpleMFRC522()
 
 while True:
-        id, text = rfid.read()
-        print("id: " + str(id))
-        print("text: " + str(text))
+    id, text = rfid.read()
+    print("id: " + str(id))
+    print("text: " + str(text))
